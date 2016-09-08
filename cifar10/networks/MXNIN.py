1fbd0b56222b741535e68c7ce4bc2a0111ebd3f6
GaiYu0 <yg1246@nyu.edu>
2016-09-08 08:23:17 +0800

    modify NIN training script.

--- cifar10/networks/MXNIN.py
+++ cifar10/networks/MXNIN.py
@@ -1,31 +1,25 @@
 import sys
+sys.path.append('../')
 sys.path.append('../../nn')
 
 import cPickle as pickle
 
-from data_utilities import *
-from GPU_utility import *
-from MXModels import *
+from utilities.data_utility import load_cifar10
+from GPU_utility import GPU_availability
+from MXModels.NIN import NIN
 from MXSolver import *
 from MXInitializer import *
-
-import sys
-m = float(sys.argv[1])
-print m
-
-data = load_cifar10(reshape=True, center=True, rescale=True)
-
-model = NIN('DReLU', DReLUInitializer())
 
 optimizer_settings = {
   'lr'                : 0.2,
   'lr_decay_interval' : 10,
   'lr_decay_factor'   : 1.0,
-  'momentum'          : m,
+  'momentum'          : 0,
   'optimizer'         : 'SGD',
   'weight_decay'      : 0
 }
 
+data = load_cifar10(reshape=True, center=True, rescale=True)
 solver_configuration = {
   'batch_size'         : 128,
   'data'               : data,
@@ -34,7 +28,10 @@ solver_configuration = {
   'optimizer_settings' : optimizer_settings,
 }
 
+model = NIN('DReLU', DReLUInitializer())
 solver = MXSolver(model, **solver_configuration)
 history = solver.train()
-path = 'NIN-{}-history'.format(optimizer_settings)
-pickle.dump(history, open('models/%s' % path, 'wb'))
+'''
+path = ''
+pickle.dump(history, open('../models/%s' % path, 'wb'))
+'''

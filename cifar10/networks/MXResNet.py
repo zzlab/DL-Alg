15eae9102bfff20c72efb5a0be4fe41fca98378f
GaiYu0 <yg1246@nyu.edu>
2016-09-17 20:46:30 +0800

    update MXResNet.py

--- cifar10/networks/MXResNet.py
+++ cifar10/networks/MXResNet.py
@@ -1,8 +1,9 @@
 import sys
+sys.path.append('../')
 sys.path.append('../../nn')
-from GPU_utility import *
 
-from data_utilities import *
+from utilities.data_utilities import load_cifar10
+from utilities.GPU_utility import GPU_availability
 
 import MXModels
 from MXInitializer import *
@@ -23,13 +24,11 @@ args = parser.parse_args()
 activation = 'BNReLU'
 n = 3
 
-model = MXModels.ResidualNetwork(n, activation, DReLUInitializer())
-
 optimizer_settings = {
-  'lr'                : 0.10,
+  'lr'                : 0.001,
   'lr_decay_interval' : 20,
   'lr_decay_factor'   : 0.9,
-  'optimizer'         : 'SGD',
+  'optimizer'         : 'Adam',
   'weight_decay'      : 0
 }
 
@@ -41,11 +40,10 @@ solver_configuration = {
   'optimizer_settings' : optimizer_settings,
 }
 
-path = 'residual-%d-%s' % (n, activation)
-
+kernel = 'double'
+model = MXModels.ResidualNetwork(n, activation, DReLUInitializer(), kernel=='double')
 solver = MXSolver.MXSolver(model, **solver_configuration)
-history, test = solver.train()
-'''
+history = solver.train() # test_accuracy, progress
+path = 'residual-%d-%s-%s-Adam' % (n, activation, kernel)
 pickle.dump(history, open('models/%s-history' % path, 'wb'))
-pickle.dump((solver.model.arg_params, solver.model.aux_params), open('models/%s-parameters' % path, 'wb'))
-'''
+# pickle.dump((solver.model.arg_params, solver.model.aux_params), open('models/%s-parameters' % path, 'wb'))

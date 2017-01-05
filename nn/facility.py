e6dcc7d4f3664960e89487ebbf877299ce4c356e
GaiYu0 <yg1246@nyu.edu>
2017-01-05 19:46:31 +0800

    nn/facility.py

--- nn/facility.py
+++ nn/facility.py
@@ -24,6 +24,48 @@ def load_mnist(path='/home/alex/experiment/mnist/utilities', shape=None):
   unpacked = tuple(unpacked)
   return unpacked
 
+def load_cifar10(path=None, reshape=False, center=False, rescale=False, validation=4):
+  import cPickle as pickle
+  import numpy as np
+  path = '/home/alex/experiment/cifar10/utilities/cifar/' if path is None else path
+  training_data, training_labels = [], []
+  for i in range(5):
+    if i != validation:
+      batch = pickle.load(open(path + 'data_batch_%d' % (i + 1), 'rb'))
+      training_data.append(batch['data'])
+      training_labels.append(np.array(batch['labels']))
+  training_data = np.vstack(training_data)
+  if center:
+    mean = training_data.mean(axis=0).reshape((1, 3072))
+    training_data = training_data - mean
+  if rescale:
+    deviation = (((training_data ** 2).mean(axis=0)) ** 0.5).reshape((1, 3072))
+    training_data = training_data / deviation
+  if reshape: training_data = training_data.reshape((40000, 3, 32, 32))
+  training_labels = np.hstack(training_labels)
+
+  if validation is None: validation_batch = pickle.load(open(path + 'test_batch', 'rb'))
+  else: validation_batch = pickle.load(open(path + 'data_batch_%d' % (validation + 1), 'rb'))
+  validation_data = np.array(validation_batch['data'])
+  if center: validation_data = validation_data - mean
+  if rescale: validation_data = validation_data / deviation
+  if reshape: validation_data = validation_data.reshape((10000, 3, 32, 32))
+  validation_labels = np.array(validation_batch['labels'])
+
+  test_batch = pickle.load(open(path + 'test_batch', 'rb'))
+  test_data = np.array(test_batch['data'])
+
+  if center: test_data = test_data - mean
+  if rescale: test_data = test_data / deviation
+  if reshape: test_data = test_data.reshape((10000, 3, 32, 32))
+  test_labels = np.array(test_batch['labels']) 
+  
+  return \
+    training_data, training_labels, \
+    validation_data, validation_labels, \
+    test_data, test_labels
+
+
 def batch_dot(left, right):
   # assert left.shape[0] == right.shape[0] and left.shape[2] == right.shape[1]
   left_symbol = symbol.Variable('left')
